1eaee9562f6e75d599f0fdcc5f9c2b657ef735b9
Anders Heed <aheed@users.noreply.github.com>
2021-01-04 20:54:35 +0100

    add mongodb connection to backend

--- backend/app.ts
+++ backend/app.ts
@@ -2,9 +2,116 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import jwt from 'jsonwebtoken';
 import { OAuth2Client } from 'google-auth-library';
+import * as dotenv from 'dotenv';
+import mongoose from "mongoose";
 var cors = require('cors')
 
 const CLIENT_ID = "354253354749-bfp5ial5k53abr4o9q5c44f66nbnkjrn.apps.googleusercontent.com";
+
+
+////////////////////
+dotenv.config();
+
+mongoose.set('useFindAndModify', false);
+
+const connectMongo = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CONN as string, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    /*const user: IUser = await User.create({
+      email: 'bill@microsoft.com',
+      firstName: 'Bill',
+      lastName: 'Gates'
+    });*/
+
+
+    console.log('Connected to DB');
+  }
+  catch(err) {
+    console.error(err);
+  }
+}
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  levels: [{
+    type: String,
+  }],
+});
+
+export interface User extends mongoose.Document {
+  id: string;
+  name: string;
+  levels: Array<string>;
+}
+
+const UserModel = mongoose.model<User>('User', userSchema);
+
+const createUser = async (userDoc: any) => {
+  let u = new UserModel(userDoc);
+  await u.save();
+}
+
+const overwriteUser = async () => {
+  return await UserModel.findOneAndUpdate({id: '456'}, {name: 'newnameww'}, {upsert: true, setDefaultsOnInsert: true});
+  //await createUser(userDoc);
+}
+
+const createUserTest = async () => {
+  await createUser({
+    id: '123',
+    name: 'alpha',
+    levels: ['aa', 'bb', 'cc']})
+  .then(() => {
+    console.log("user saved");
+  })
+  .catch((reason) => {
+    console.error("failed to save user to DB");
+    console.error(reason);
+  });
+
+  /*await createUser({
+    apa: 536,
+    })
+  .then(() => {
+    console.log("user saved2");
+  })
+  .catch((reason) => {
+    console.error("failed to save user to DB2");
+    console.error(reason);
+  });*/
+
+
+  let res = await overwriteUser()
+  .then((r) => {
+    console.log("user overwritten");
+    console.log(r);
+  })
+  .catch((reason) => {
+    console.error("failed to overwrite user");
+    console.error(reason);
+  });
+  console.log(res);
+}
+
+connectMongo()
+  .then(() => {
+    createUserTest();
+  });
+
+
+
+////////////////////
 
 const app: express.Application = express();
 

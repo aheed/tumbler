8ee2bba6443728c94494d349d330ac0e1d50444f
aheed <anders.heed@combination.se>
2021-02-21 16:34:42 +0100

    bit ball animation start of impl

--- src/components/Bit.tsx
+++ src/components/Bit.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from "react";
 import { TumblerBit } from "../logic/TumblerBit";
 import { TumblerEvent, TumblerEventType } from "../logic/TumblerEvent";
+import { TumblerBallColor } from "../logic/TumblerTypes";
 import './Bit.css';
 
 interface BitProps {
@@ -23,10 +24,23 @@ export const Bit : React.FC<BitProps> = ({bit}) => {
 
         const onObserveEvent = async (evt: TumblerEvent) => {
             console.log(`bit event: ${TumblerEventType[evt.eventType]}`);
+
+            ballRef.current?.classList.add(evt.ballColor === TumblerBallColor.Blue ? 'ball-blue' : 'ball-red');
+            let transitClass = evt.enterLeft === evt.exitLeft ? 'transit-down' : 'transit-across';
+            ballRef.current?.classList.add(transitClass);
+            //ballRef.current?.classList.add('in-transit');
+            if (!evt.enterLeft) {
+                ballRef.current?.classList.add('reverse');
+            }
             
             updateBitState();
 
-            await new Promise(r => setTimeout(r, 500));
+            await new Promise(r => setTimeout(r, 350));
+
+            ballRef.current?.classList.remove('in-transit');
+            ballRef.current?.classList.remove('transit-down');
+            ballRef.current?.classList.remove('transit-across');
+            ballRef.current?.classList.remove('reverse');
             
             return;
         }
@@ -36,9 +50,15 @@ export const Bit : React.FC<BitProps> = ({bit}) => {
     }, [bit]);
 
     let imgRef = useRef<HTMLImageElement>(null);
+    let ballRef = useRef<HTMLDivElement>(null);
 
     return (
         <div className={`bit-outer`}>
+            <div className='ball-outer' ref={ballRef}>
+                <svg className='ball' height="80" width="80">
+                    <circle className='ball-circle' cx="15" cy="15" r="7" stroke="black" strokeWidth="1" fill="red" />
+                </svg>
+            </div>
             <img className={`bit`} src='./emptypart.png' alt='empty'></img>
             <img className={`bit`} src='./bit.png' alt='bit' ref={imgRef}></img>
         </div>
--- src/logic/TumblerBit.ts
+++ src/logic/TumblerBit.ts
@@ -9,15 +9,21 @@ export class TumblerBit extends TumblerPart {
         super(TumblerPartType.Bit, facingLeft, leftExit, rightExit);
         this.set = facingLeft;
 
-        this.leftEntrance = this.rightEntrance = {
-            putBall: async (color : TumblerBallColor): Promise<TumblerResult> => {
-                let evtType = this.set ? TumblerEventType.Reset : TumblerEventType.Set;
-                let evt = new TumblerEvent(evtType, TumblerPartType.Bit);
-                let currentExit = this.set ? this.leftExit : this.rightExit;
-                this.set = !this.set;
-                await this.reportEvent(evt);
-                return currentExit.putBall(color);
-            }
+        const putBallHandler = (enterLeft: boolean) => async (color : TumblerBallColor): Promise<TumblerResult> => {
+            let evtType = this.set ? TumblerEventType.Reset : TumblerEventType.Set;
+            let evt = new TumblerEvent(evtType, TumblerPartType.Bit, color, enterLeft, this.set);
+            let currentExit = this.set ? this.leftExit : this.rightExit;
+            this.set = !this.set;
+            await this.reportEvent(evt);
+            return currentExit.putBall(color);
+        }
+
+        this.leftEntrance = {
+            putBall: putBallHandler(true)
+        }
+
+        this.rightEntrance = {
+            putBall: putBallHandler(false)
         }
     }
 }

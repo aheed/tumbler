744cf503bc525dac456d34eb110552ee7c00ece2
aheed <anders.heed@combination.se>
2021-02-21 18:29:20 +0100

    add crossover ball animation

--- src/components/Crossover.tsx
+++ src/components/Crossover.tsx
@@ -1,6 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { TumblerCrossover } from "../logic/TumblerCrossover";
 import { TumblerEvent, TumblerEventType } from "../logic/TumblerEvent";
+import { TumblerBallColor } from "../logic/TumblerTypes";
+import './Crossover.css';
 
 interface CrossoverProps {
     crossover: TumblerCrossover,
@@ -12,7 +14,16 @@ export const Crossover : React.FC<CrossoverProps> = ({crossover}) => {
         const onObserveEvent = async (evt: TumblerEvent) => {
             console.log(`crossover event: ${TumblerEventType[evt.eventType]}`);
 
-            await new Promise(r => setTimeout(r, 400));
+            ballRef.current?.classList.add(evt.ballColor === TumblerBallColor.Blue ? 'ball-blue' : 'ball-red');
+            ballRef.current?.classList.add('transit-across');
+            if (!evt.enterLeft) {
+                ballRef.current?.classList.add('reverse');
+            }
+
+            await new Promise(r => setTimeout(r, 350));
+
+            ballRef.current?.classList.remove('transit-across');
+            ballRef.current?.classList.remove('reverse');
             
             return;
         }
@@ -20,8 +31,15 @@ export const Crossover : React.FC<CrossoverProps> = ({crossover}) => {
         crossover.addObserver({reportEvent: onObserveEvent})
     }, [crossover]);
 
+    let ballRef = useRef<HTMLDivElement>(null);
+
     return (
         <div className={`crossover-outer`}>
+            <div className='ball-outer' ref={ballRef}>
+                <svg className='ball' height="100" width="100">
+                    <circle className='ball-circle' cx="15" cy="15" r="7" stroke="black" strokeWidth="1" fill="red" />
+                </svg>
+            </div>
             <img className={`crossover`} src='./crossover.png' alt='crossover'></img>
         </div>
     );

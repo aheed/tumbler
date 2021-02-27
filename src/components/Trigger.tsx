762638e730aa636abd4202d58affc607caa3b075
aheed <anders.heed@combination.se>
2021-02-27 18:43:40 +0100

    add support for disabling triggers during execution

--- src/components/Trigger.tsx
+++ src/components/Trigger.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import { IBallReceiver, TumblerBallColor } from '../logic/TumblerTypes';
+import { AppContext, AppStatus } from '../services/AppContext';
 
 
 interface TriggerProps {
-    receiver: IBallReceiver
+    receiver: IBallReceiver,
     releaseButtonText: string
 }
 
-export const Trigger : React.FC<TriggerProps> = ({receiver, releaseButtonText}) => {
+interface TriggerInnerProps extends TriggerProps {
+    enabled: boolean,
+    setAppStatus: (appStatus: AppStatus) => void
+}
 
-    const onTrig = () => {
-        receiver.putBall(TumblerBallColor.Blue);
+export const TriggerInner : React.FC<TriggerInnerProps> = ({receiver, releaseButtonText, enabled, setAppStatus}) => {
+
+    const onTrig = async () => {
+        setAppStatus(AppStatus.Executing);
+        await receiver.putBall(TumblerBallColor.Blue);
+        setAppStatus(AppStatus.Idle);
     }
 
     return (
-        <button onClick={onTrig}>{releaseButtonText}</button>
+        <button onClick={onTrig} disabled={!enabled}>{releaseButtonText}</button>
+    );
+}
+
+export const Trigger : React.FC<TriggerProps> = ({receiver, releaseButtonText}) => {
+    return (
+        <AppContext.Consumer>
+        {(appState) => (
+            <TriggerInner receiver={receiver} releaseButtonText={releaseButtonText} enabled={appState.appStatus === AppStatus.Idle} setAppStatus={appState.setAppStatus}></TriggerInner>
+        )}
+        </AppContext.Consumer>
     );
 }
